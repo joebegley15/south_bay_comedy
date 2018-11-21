c38266eb5eaa91aa9c0f5db85804ea5276d1efa6
Joe Begley <joebegley15@gmail.com>
2018-11-21 13:29:23 -0500

    Preparing for a full backend implementation

--- client/src/reducers/showReducer.js
+++ client/src/reducers/showReducer.js
@@ -2,57 +2,7 @@ import uuid from "uuid";
 import { GET_SHOWS, ADD_SHOW, DELETE_SHOW } from "../actions/types";
 
 const initialState = {
-  shows: [
-    {
-      id: uuid(),
-      title: "Pizza Factory",
-      day: "Sunday",
-      start: "8:00 PM",
-      end: "10:00 PM",
-      address: "3039 Meridian Ave #44, San Jose, CA 95124, United States",
-      warnings: {
-        clean: { val: false, message: "Clean show" },
-        runslate: { val: false, message: "Runs late" },
-        mixed: {
-          val: false,
-          message: "Mixed mic. Musicians and poets welcome."
-        },
-        presignup: { val: true, message: "Online signup available" },
-        presignupmand: { val: false, message: "Must sign up in advance" },
-        prebooked: { val: false, message: "Booked in advnce" }
-      },
-      schedule: "Weekly",
-      hosts: ["PX Floro", "Calvin Haha"],
-      notes: "Produced and hosted by PX Floro, featuring Calvin Haha."
-    },
-    {
-      id: uuid(),
-      title: "Poor House Bistro",
-      day: "Monday",
-      start: "6:00 PM",
-      end: "9:00 PM",
-      address: "91 S Autumn St, San Jose, CA 95110, USA",
-      warnings: {
-        clean: { val: true, message: "Clean show" },
-        runslate: { val: false, message: "Runs late" },
-        mixed: {
-          val: false,
-          message: "Mixed mic. Musicians and poets welcome."
-        },
-        presignup: { val: false, message: "Online signup available" },
-        presignupmand: { val: false, message: "Must sign up in advance" },
-        prebooked: { val: false, message: "Booked in advnce" }
-      },
-      schedule: "weekly",
-      hosts: [
-        "Dave Allender",
-        "Cap Wilhelm-Safian",
-        "Ian Davis",
-        "Steph Frazier"
-      ],
-      notes: "6 pm signup during 30-minute opening music. Good food and drinks."
-    }
-  ]
+  shows: []
 };
 
 export default function(state = initialState, action) {
@@ -64,6 +14,7 @@ export default function(state = initialState, action) {
     case DELETE_SHOW:
       console.log(state.shows.filter(item => item.id != action.payload));
       return {
+        ...state,
         shows: state.shows.filter(item => item.id != action.payload)
       };
     default:
